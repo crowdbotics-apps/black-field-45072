fbe8b390772c74e1a6afa10d53be18a7346f8737
Crowdbotics <team@crowdbotics.com>
2023-12-18 07:53:33 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "black-field-45072",
+  url: "https://black-field-45072.botics.co",
+  api: "https://black-field-45072.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
